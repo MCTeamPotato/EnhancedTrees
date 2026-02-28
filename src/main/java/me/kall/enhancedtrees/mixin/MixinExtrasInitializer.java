026a87872414e59f793626a4b8e5d63c8a728fac
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-02-28 23:43:31 +0800

    1211 neo port

--- src/main/java/me/kall/enhancedtrees/mixin/MixinExtrasInitializer.java
+++ src/main/java/me/kall/enhancedtrees/mixin/MixinExtrasInitializer.java
@@ -1,7 +1,7 @@
 package me.kall.enhancedtrees.mixin;
 
 import com.llamalad7.mixinextras.MixinExtrasBootstrap;
-import net.minecraftforge.fml.loading.LoadingModList;
+import net.neoforged.fml.loading.LoadingModList;
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
