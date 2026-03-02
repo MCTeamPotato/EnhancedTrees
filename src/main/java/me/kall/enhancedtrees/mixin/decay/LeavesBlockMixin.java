3318ef54ae7ec8d226b2674b9c5d25ced37c08f3
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-03-02 17:13:08 +0800

    fix

--- src/main/java/me/kall/enhancedtrees/mixin/decay/LeavesBlockMixin.java
+++ src/main/java/me/kall/enhancedtrees/mixin/decay/LeavesBlockMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(LeavesBlock.class)
 public abstract class LeavesBlockMixin {
-    @ModifyConstant(method = {"decaying", "isRandomlyTicking", "<init>"}, constant = @Constant(intValue = 7))
+    @ModifyConstant(method = {"isRandomlyTicking", "<init>", "randomTick"}, constant = @Constant(intValue = 7))
     private int enhancedTrees$decaying(int constant) {
         return (int) (constant * TreeBonusConfig.FOLIAGE_DECAY_RANGE_BONUS);
     }
